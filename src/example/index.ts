a55d666b106e8a12e3aa8449db21c8d780f23d39
Bishwa Thapa <15176360+thapabishwa@users.noreply.github.com>
2023-04-21 10:03:20 +0545

    Add support for Path Parameters (#17)
    * feat: add support for path parameters
    
    * chore: remove undefined as it's not needed anymore
    
    * chore: add examples
    
    * chore: bump major version
    
    ---------
    
    Co-authored-by: Bishwa Thapa <bishwa.thapa@plerion.com>

--- src/example/index.ts
+++ src/example/index.ts
@@ -3,13 +3,15 @@ import { LocalLambda, LocalLambdaConfig } from '../local.lambda';
 import { LambdaResponse, RequestEvent } from '../types';
 
 // handler is a function that takes in an event and context and returns a response
-const handler = async (req: RequestEvent, context: Context): Promise<LambdaResponse> => ({ statusCode: 200, body: 'Hello World' });
+const handler = async (req: RequestEvent, context: Context): Promise<LambdaResponse> => ({ statusCode: 200, body: `Hello World !!! My userId is ${req.pathParameters?.id}\n` });
 
 // context is provided as optional field in config.
 const config: LocalLambdaConfig = {
   handler, // if type is not compatible, do `handler: handler as any`
   port: 8000, // optional, default to 8000
+  pathParamsPattern: '/user/:id', // optional, default to '/'
 };
 
+// visit http://localhost:8080/user/1234567890 to see the response
 const localLambda = new LocalLambda(config);
 localLambda.run();
